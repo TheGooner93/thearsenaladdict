9f9149efe8cf3c86d421377ff8f4634e62e9e50e
Mohammed Ehab <mohammedehab93@gmail.com>
2017-12-15 19:48:49 +0530

    first backend commit

---
+++ server.js
@@ -0,0 +1,51 @@
+// var http = require('http');
+// http.createServer(function(req,res){
+//     res.writeHead(200,{'Content-Type':'text/plain'});
+//     res.end('Hello New York\n');
+// }).listen(3001);
+
+// console.log('Server running at localhost:3001/');
+
+var express = require('express');
+var ical2json = require('ical2json');
+var fs = require('fs');
+var download = require('download-file');
+
+var app = express();
+var url = 'https://ics.fixtur.es/v2/arsenal.ics?2e20f9b87f16bdb8';
+var arsenalCalendarJSON = {};
+var options = {
+    directory: "./",
+    filename: "arsenal.ics"
+}
+
+//On start of server, download the ICS and build JSON
+download(url, options, function (err) {
+    if(!err){
+        console.log("Error:" + err);
+        arsenalCalendarJSON = ical2json.convert(fs.readFileSync('./arsenal.ics').toString());
+    }
+});
+
+// app.configure(function(){
+//     app.use(express.bodyParser());
+// });
+
+app.get('/', function (req, res) {
+    res.send('Change the URL to see fixtures');
+});
+
+app.get('/arsenal', function (req, res) {
+    
+    var aFixtures = arsenalCalendarJSON.VCALENDAR[0].VEVENT;
+    // aFixtures.forEach(function (fixture,index) {
+    //     if(fixture.DTSTART < new Date()){
+    //         aFixtures.splice(index,1);
+    //     }
+    // });
+    res.send(aFixtures);
+});
+
+app.listen(3001);
+
+console.log('SHE WORE SHE WORE..');
