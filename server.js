39bd2b6417bf72e0d10df0c3fdae29aef761754d
Mohammed Ehab <mohammedehab93@gmail.com>
2019-04-02 20:42:45 +0400

    Fetching only future dated fixtures

--- server.js
+++ server.js
@@ -1,53 +1,69 @@
-// var http = require('http');
-// http.createServer(function(req,res){
-//     res.writeHead(200,{'Content-Type':'text/plain'});
-//     res.end('Hello New York\n');
-// }).listen(3001);
-
-// console.log('Server running at localhost:3001/');
-
-var express = require('express');
-var ical2json = require('ical2json');
-var fs = require('fs');
-var download = require('download-file');
+var express = require("express");
+var ical2json = require("ical2json");
+var fs = require("fs");
+var download = require("download-file");
+var parseIcalDate = require("ical-date-parser");
 
 var app = express();
-var url = 'https://ics.fixtur.es/v2/arsenal.ics?fba191619381b181'; /*unofficial*/
+var url =
+  "https://ics.fixtur.es/v2/arsenal.ics?fba191619381b181"; /*unofficial*/
 // var url = 'https://ics.ecal.com/ecal-sub/5ca33a74f8f200d45b8b4567/Arsenal%20FC.ics'; /*official*/
 var arsenalCalendarJSON = {};
+var fileReadPromise;
 var options = {
-    directory: "./",
-    filename: "arsenal.ics"
-}
+  directory: "./",
+  filename: "arsenal.ics"
+};
 
 //On start of server, download the ICS and build JSON
-download(url, options, function (err) {
-    if(!err){
-        arsenalCalendarJSON = ical2json.convert(fs.readFileSync('./arsenal.ics').toString());
-    } else { 
+download(url, options, function(err) {
+  if (!err) {
+    // arsenalCalendarJSON = ical2json.convert(fs.readFileSync('./arsenal.ics').toString());
+    fileReadPromise = new Promise(function(resolve, reject) {
+      var oFileContents = fs.readFile("./arsenal.ics", (err, data) => {
+        if (err) {
+          throw err;
+        }
+        resolve(data);
+      });
+    });
+    fileReadPromise
+      .then(function(fileContents) {
+        arsenalCalendarJSON = ical2json.convert(fileContents.toString());
+      })
+      .catch(function(err) {
         console.log("Error:" + err);
-    }
+      });
+  } else {
+    throw err;
+  }
 });
 
-// app.configure(function(){
-//     app.use(express.bodyParser());
-// });
-
-app.get('/', function (req, res) {
-    res.send('Change the URL to see fixtures');
+app.get("/", function(req, res) {
+  res.send("Change the URL to see fixtures");
 });
 
-app.get('/arsenal', function (req, res) {
-    
-    var aFixtures = arsenalCalendarJSON.VCALENDAR[0].VEVENT;
-    // aFixtures.forEach(function (fixture,index) {
-    //     if(fixture.DTSTART < new Date()){
-    //         aFixtures.splice(index,1);
-    //     }
-    // });
-    res.send(aFixtures);
+app.get("/arsenal", function(req, res) {
+  var currentDate = new Date();
+  var aFixtures = arsenalCalendarJSON.VCALENDAR[0].VEVENT;
+  // if(new Date(parseIcalDate(aFixtures[0].DTSTART)) >= new Date()){
+  var finalFixtures = aFixtures
+    .filter(fixture => {
+      return new Date(parseIcalDate(fixture.DTSTART)) >= currentDate;
+    })
+    .map(fixture => {
+      return (fixture = {
+        ...fixture,
+        DTSTART: parseIcalDate(fixture["DTSTART"]),
+        DTEND: parseIcalDate(fixture["DTEND"])
+      });
+    });
+  res.send(finalFixtures);
+  // } else {
+  //     res.send("Sorry");
+  // }
 });
 
 app.listen(3001);
 
-console.log('SHE WORE SHE WORE..');
+console.log("SHE WORE SHE WORE..");
