281229620f9d03b6e2018190a05236895706d058
Mohammed Ehab <mohammedehab93@gmail.com>
2019-04-02 20:43:18 +0400

    Fetching only future dated fixtures

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ var parseIcalDate = require("ical-date-parser");
 var app = express();
 var url =
   "https://ics.fixtur.es/v2/arsenal.ics?fba191619381b181"; /*unofficial*/
-// var url = 'https://ics.ecal.com/ecal-sub/5ca33a74f8f200d45b8b4567/Arsenal%20FC.ics'; /*official*/
 var arsenalCalendarJSON = {};
 var fileReadPromise;
 var options = {
@@ -18,7 +17,6 @@ var options = {
 //On start of server, download the ICS and build JSON
 download(url, options, function(err) {
   if (!err) {
-    // arsenalCalendarJSON = ical2json.convert(fs.readFileSync('./arsenal.ics').toString());
     fileReadPromise = new Promise(function(resolve, reject) {
       var oFileContents = fs.readFile("./arsenal.ics", (err, data) => {
         if (err) {
@@ -46,7 +44,6 @@ app.get("/", function(req, res) {
 app.get("/arsenal", function(req, res) {
   var currentDate = new Date();
   var aFixtures = arsenalCalendarJSON.VCALENDAR[0].VEVENT;
-  // if(new Date(parseIcalDate(aFixtures[0].DTSTART)) >= new Date()){
   var finalFixtures = aFixtures
     .filter(fixture => {
       return new Date(parseIcalDate(fixture.DTSTART)) >= currentDate;
@@ -59,9 +56,6 @@ app.get("/arsenal", function(req, res) {
       });
     });
   res.send(finalFixtures);
-  // } else {
-  //     res.send("Sorry");
-  // }
 });
 
 app.listen(3001);
