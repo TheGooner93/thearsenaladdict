b81c9aa27537dbb5d9c914597ac86b6139dfe5ba
Mohammed Ehab <mohammedehab93@gmail.com>
2019-04-27 18:47:17 +0400

    Spinner logic changed

--- client/src/components/FixtureList.js
+++ client/src/components/FixtureList.js
@@ -26,22 +26,22 @@ class FixtureList extends Component {
               </Col>
             </Row>
             <Row>
+              {isLoading ? (
+                <Col xs="12" sm="12" xl="12">
+                  <Spinner />
+                </Col>
+              ) : (
+                ""
+              )}
               {latestFixture && Object.keys(latestFixture).length ? (
                 <Col xs="12" sm="12" xl="12">
                   <FixtureCard fixture={latestFixture} />
                 </Col>
               ) : (
                 <Col xs="12" sm="12" xl="12">
-                  {isLoading ? (
-                    <Spinner />
-                  ) : (
-                    <div
-                      className="container mt-1"
-                      style={{ textAlign: "left" }}
-                    >
-                      <h3>No Available Fixture</h3>
-                    </div>
-                  )}
+                  <div className="container mt-1" style={{ textAlign: "left" }}>
+                    <h3>No Available Fixture</h3>
+                  </div>
                 </Col>
               )}
             </Row>
@@ -53,6 +53,13 @@ class FixtureList extends Component {
               </Col>
             </Row>
             <Row>
+              {isLoading ? (
+                <Col xs="12" sm="12" xl="12">
+                  <Spinner />
+                </Col>
+              ) : (
+                ""
+              )}
               {futureFixtures && futureFixtures.length ? (
                 futureFixtures.map(fixture => (
                   <Col key={fixture.UID} xs="12" sm="6" xl="4">
@@ -61,16 +68,9 @@ class FixtureList extends Component {
                 ))
               ) : (
                 <Col xs="12" sm="12" xl="12">
-                  {isLoading ? (
-                    <Spinner />
-                  ) : (
-                    <div
-                      className="container mt-1"
-                      style={{ textAlign: "left" }}
-                    >
-                      <h3>No Scheduled Fixtures</h3>
-                    </div>
-                  )}
+                  <div className="container mt-1" style={{ textAlign: "left" }}>
+                    <h3>No Scheduled Fixtures</h3>
+                  </div>
                 </Col>
               )}
             </Row>
