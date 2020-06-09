e52f97d8f5adb89eaf6d92a3b8485d1a880754ba
Mohammed Ehab <mohammedehab93@gmail.com>
2020-06-09 12:54:03 +0400

    Added react-jss

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,15 +1,23 @@
 import React, { Component } from "react";
 import { Container, Navbar, NavbarBrand } from "reactstrap";
+import injectSheet from 'react-jss';
+import classNames from 'classnames';
 
 class AllFixtures extends Component {
   render() {
+    const {
+      classes : {
+        imageStyle = {}
+      }
+    } = this.props;
+    
     return (
       <Container>
         <Navbar light className="mb-3 rounded app-header">
           <NavbarBrand href="/">
             <h2>The Arsenal Addict</h2>
           </NavbarBrand>
-          <div className="level-right">
+          <div className={classNames("level-right"), imageStyle}>
             {" "}
             <img
               src="../arsenal-icon.png"
@@ -23,4 +31,12 @@ class AllFixtures extends Component {
   }
 }
 
-export default AllFixtures;
+const jssStyles = {
+  imageStyle : {
+    '@media (max-width: 490px)': {
+      display: 'none'
+    }
+  }
+}
+
+export default injectSheet(jssStyles)(AllFixtures);
